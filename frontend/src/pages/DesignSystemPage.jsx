e19ba7d308c5053f3ed384a0f7ff6d2d0c0fa564
Dika Laksana <dikalaksana@MacBook-Air-Dika.local>
2026-07-26 19:56:06 +0700

    Revert "feat: adopt Balanja brand mark"
    This reverts commit 2bc2c42335706c2eef0ad7a62ef610d456e27616.

--- frontend/src/pages/DesignSystemPage.jsx
+++ frontend/src/pages/DesignSystemPage.jsx
@@ -27,7 +27,6 @@ import NavigationPatternsShowcase from "../components/design/NavigationPatternsS
 import MasterDataPatternsShowcase from "../components/design/MasterDataPatternsShowcase.jsx";
 import OrganizationOnboardingShowcase from "../components/design/OrganizationOnboardingShowcase.jsx";
 import EntitlementPatternsShowcase from "../components/design/EntitlementPatternsShowcase.jsx";
-import BrandPatternsShowcase from "../components/design/BrandPatternsShowcase.jsx";
 
 export default function DesignSystemPage({ onNavigate }) {
   return (
@@ -59,7 +58,6 @@ export default function DesignSystemPage({ onNavigate }) {
             operational spacing for UMKM cashier workflows.
           </p>
         </div>
-        <BrandPatternsShowcase />
         <TokenGrid />
         <TypographyPanel />
         <MarketingPatternsShowcase />
